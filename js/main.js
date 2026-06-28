467edc1a538ae4c8f5175628fc031089b3c9c487
pabloandino0804-bit <pablo.andino0804@gmail.com>
2026-06-28 15:50:24 -0300

    feat: agregar boton de modo oscuro en la paginas

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
-const botonColaborar = document.querySelector(".inicio-btn")
-const seccionAyuda = document.getElementById("como-ayudar-index")
+// Modo Oscuro
+const boton = document.getElementById("btn-modo");
+boton.addEventListener("click", () => {
+    document.body.classList.toggle("oscuro");
+});
+
+//
+const botonColaborar = document.querySelector(".inicio-btn");
+const seccionAyuda = document.getElementById("como-ayudar-index");
 
 
-const botonProgramas = document.querySelector(".btn-programas")
+const botonProgramas = document.querySelector(".btn-programas");
 
 botonColaborar.addEventListener('click', function (e) {
     e.preventDefault()
     seccionAyuda.scrollIntoView({ behavior: 'smooth' })
-})
+});
 
 botonProgramas.addEventListener('click', function () {
     window.location.href = 'programas.html'
-})
+});
